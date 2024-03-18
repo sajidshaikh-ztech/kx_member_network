a53a8f9e52e90dcb2a27c4c3e21063bebe9f271b
sajidshaikh-ztech <sajid.shaikh@ey.com>
2024-03-17 21:52:05 -0400

    Code Commit

--- src/App.js
+++ src/App.js
@@ -2,17 +2,13 @@ import React from 'react';
 import Header from "./Header";
 import Body from "./Body";
 import Footer from "./Footer"
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import RegisteredUserPage from "./RegisteredUserPage";
-import SignInPage from "./SignInPage";
-
 function App() {
   return (
-          <div>
-              <RegisteredUserPage/>
-
-          </div>
-
+      <div className="App">
+        <Header />
+        <Body />
+        <Footer />
+      </div>
   );
 }
 
