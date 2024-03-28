75c1de88c2554f0f12a224a9966d7aec4f695061
sajidshaikh-ztech <sajid.shaikh@ey.com>
2024-03-28 12:47:13 -0400

    Code Commit

--- src/GuestUserPage.jsx
+++ src/GuestUserPage.jsx
@@ -1,11 +1,9 @@
 import React, { useState } from 'react';
-import {Button, Container, TextField} from '@mui/material';
-import Header from "./Header";
-import Footer from "./Footer";
-import Body from "./Body";
+import {Container} from '@mui/material';
+
 import GuestBody from "./GuestBody";
 
-function RegisteredUserPage() {
+function GuestUserPage() {
     return (
        <Container>
             <GuestBody/>
@@ -13,4 +11,4 @@ function RegisteredUserPage() {
     );
 }
 
-export default RegisteredUserPage;
+export default GuestUserPage;
